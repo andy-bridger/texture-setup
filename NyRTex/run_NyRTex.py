fixed rot1 rotation scheme

NyRTex/run_NyRTex.py
@@ -37,7 +37,7 @@ drs = [np.moveaxis(np.concatenate((np.load(f"{det_r_dir}/ENOR00{run}.npy").sum(a
 
 exp_data = ExperimentalData(detectors, 'Cu_bolt', from_data= False)
 exp_data.detector_readouts = drs
-exp_data.smps = [Goniometer(1, int(ang[1]), int(ang[1]) ,int(ang[2]), scheme = 'rot1' ) for ang in angs]
+exp_data.smps = [Goniometer(1, int(ang[0]), int(ang[1]) ,int(ang[2]), scheme = 'rot1' ) for ang in angs]
 
 source = Source(np.load(f"{info_dir}/source_pos.npy"))
 sample = SampleObject((0.,0.,0.), GenericStateMatrixProvider(np.eye(3), np.zeros(3)), mesh = mesh.Mesh.from_file(f"{info_dir}/mesh.stl"))

goniometer.py
@@ -25,17 +25,18 @@ class GenericStateMatrixProvider:
 class Goniometer(GenericStateMatrixProvider):
     def __init__(self, scale = 1, phi =0,theta=0, psi=0, exp_runs = (), offset = np.zeros(3), scheme = 'euler'):
         super().__init__(None, None)
-        self.scheme = scheme
+        self.scheme = scheme.lower()
         self.phi = phi
         self.theta = theta
         self.psi = psi
+        self.init_rot_ax()
+        if self.scheme == 'euler':
+            self.update_norms = self.update_norms_euler
+            self.update_rot = self.update_rot_euler
+        else:
+            self.update_norms = self.update_norms_rot1
+            self.update_rot = self.update_rot_rot1
         self.update_rot()
-        self.z_norm_init = np.array((0,0,1))
-        self.x_prime_norm_init = np.array((1,0,0))
-        self.z_prime_norm_init = np.array((0,0,1))
-        self.z_norm = np.array((0,0,1))
-        self.x_prime_norm = np.array((1,0,0))
-        self.z_prime_norm = np.array((0,0,1))
         self.scale = scale
         self.update_equators()
         self.update_fracs()
@@ -45,32 +46,53 @@ class Goniometer(GenericStateMatrixProvider):
     def equator(self, r = 1.0, res = 100, offset=(0,0,0)):
         return equator(r*self.scale, res, offset)
 
-    def update_norms(self):
+    def init_rot_ax(self):
+        if self.scheme == 'euler':
+            self.ax1_init = np.array((0, 0, 1))
+            self.ax2_init = np.array((1, 0, 0))
+            self.ax3_init = np.array((0, 0, 1))
+            self.ax1 = np.array((0, 0, 1))
+            self.ax2 = np.array((1, 0, 0))
+            self.ax3 = np.array((0, 0, 1))
+        else:
+            self.ax1_init = np.array((0, 1, 0))
+            self.ax2_init = np.array((0, 0, 1))
+            self.ax3_init = np.array((0, 0, 1))
+            self.ax1 = np.array((0, 1, 0))
+            self.ax2 = np.array((0, 0, 1))
+            self.ax3 = np.array((0, 0, 1))
+
+    def update_norms_euler(self):
         rot1 = Rotation.from_euler('Z', self.phi, degrees=True)
         rot2 = Rotation.from_euler('ZX', [self.phi, self.theta], degrees=True)
-        self.x_prime_norm = rot1.apply(self.x_prime_norm_init)
-        self.z_prime_norm = rot2.apply(self.z_prime_norm_init)
+        self.ax2 = rot1.apply(self.ax2_init)
+        self.ax3 = rot2.apply(self.ax3_init)
+
+    def update_norms_rot1(self):
+        rot1 = Rotation.from_euler('yz', [self.phi, self.theta], degrees=True)
+        self.ax3 = rot1.apply(self.ax3_init)
 
     def update_equators(self):
-        self.z_eq = orient_to_pole(self.equator(r = 3), self.z_norm)
-        self.x_prime_eq = orient_to_pole(self.equator(r = 2.95), self.x_prime_norm)
-        self.z_prime_eq = orient_to_pole(self.equator(r = 2.9), self.z_prime_norm)
+        self.ax1_eq = orient_to_pole(self.equator(r = 3), self.ax1)
+        self.ax2_eq = orient_to_pole(self.equator(r = 2.95), self.ax2)
+        self.ax3_eq = orient_to_pole(self.equator(r = 2.9), self.ax3)
 
     def update_fracs(self):
-        self.phi_frac = int((self.phi/360)*self.z_eq.shape[1])
-        self.theta_frac = int((self.theta/360)*self.x_prime_eq.shape[1])
-        self.psi_frac = int((self.psi/360)*self.z_prime_eq.shape[1])
+        self.phi_frac = int((self.phi/360)*self.ax1_eq.shape[1])
+        self.theta_frac = int((self.theta/360)*self.ax2_eq.shape[1])
+        self.psi_frac = int((self.psi/360)*self.ax3_eq.shape[1])
 
-    def update_rot(self):
-        if self.scheme == 'euler':
-            self.rot = Rotation.from_euler('ZXZ', (self.phi, self.theta, self.psi), degrees=True)
-            self.rot_mat = self.rot.as_matrix()
-        else:
-            init_rot = Rotation.from_euler('yz', (self.phi, self.theta), degrees=True)
-            final_rotvec = init_rot.apply(np.array((0,0,1)))*(self.psi/360)
-            second_rot = Rotation.from_rotvec(final_rotvec, degrees=False)
-            self.rot_mat = second_rot.as_matrix()@init_rot.as_matrix()
-            self.rot = Rotation.from_matrix(self.rot_mat)
+
+    def update_rot_euler(self):
+        self.rot = Rotation.from_euler('ZXZ', (self.phi, self.theta, self.psi), degrees=True)
+        self.rot_mat = self.rot.as_matrix()
+
+    def update_rot_rot1(self):
+        init_rot = Rotation.from_euler('yz', (self.phi, self.theta), degrees=True)
+        final_rotvec = init_rot.apply(np.array((0,0,1)))*(self.psi*np.pi/180)
+        second_rot = Rotation.from_rotvec(final_rotvec, degrees=False)
+        self.rot_mat = second_rot.as_matrix()@init_rot.as_matrix()
+        self.rot = Rotation.from_matrix(self.rot_mat)
 
     def update_angles(self, phi, theta, psi):
         self.phi = phi

mantex_run.py
@@ -31,9 +31,9 @@ class GetSinglePoleFigure:
         return self.mantex.get_pole_figure_intensities(self.spectra)
 
 class GetAllPoleFigures:
-    def __init__(self, exp_data, source, sample, sample_view_axis = (1,0,0),  q_probe = 1, probe_window = 0.05):
-        self.sample_view_axis = np.asarray(sample_view_axis)
-        self.spfs = [GetSinglePoleFigure(exp_data, source, sample, sample_view_axis ,  q_probe , probe_window , ind = i) for i in range(len(exp_data.smps))]
+    def __init__(self, exp_data, source, sample, sample_view_axes = ((1,0,0),(0,1,0)),  q_probe = 1, probe_window = 0.05):
+        self.sample_view_axes = np.asarray(sample_view_axes)
+        self.spfs = [GetSinglePoleFigure(exp_data, source, sample, sample_view_axes ,  q_probe , probe_window , ind = i) for i in range(len(exp_data.smps))]
     def execute(self):
         pfis = [spf.execute() for spf in self.spfs]
         pfis = np.concatenate(pfis, axis = 0)

run_sim.py
@@ -20,9 +20,18 @@ ndet2 = Detector([5*np.sqrt(2), 5*np.sqrt(2), 0], 'north2')
 sdet = Detector([0, -10, 0], 'south')
 source = Source([20, 0 , 0])
 
-exp_runs = [(phi, theta, 0) for theta in np.arange(0, 360, 45) for phi in np.arange(0, 360, 45) ]
+# default runs
 
-goniometer = Goniometer(exp_runs = exp_runs, offset=np.array((0,0,-7)))
+#exp_runs = [(phi, theta, 0) for theta in np.arange(0, 360, 45) for phi in np.arange(0, 360, 45) ]
+
+#test loading some real runs
+
+with open(f"C:/Users/kcd17618/Documents/NyRTex/DDSteel/DDSteel_info/rotations.txt", 'r') as f:
+    angs = [x.rstrip('\n').split('\t') for x in f.readlines()]
+
+exp_runs = [(int(ang[0]), int(ang[1]) ,int(ang[2])) for ang in angs]
+
+goniometer = Goniometer(exp_runs = exp_runs, offset=np.array((0,0,-7)), scheme='rot1')
 
 sample = Sample([0,0,0], goniometer,
                 (1,1,1), 2,
@@ -31,12 +40,12 @@ sample = Sample([0,0,0], goniometer,
 
 
 model = MantexSim(source,
-              [ndet,ndet2, sdet],
+              [ndet, sdet],
               sample,
               goniometer,
               (0.7, 1.4),
-                  ((0,1,0), (0,0,1)),
-              detector_colors=('red','green', 'blue'), ewald_steps=100)
+                  ((0,1,0), (1,0,0)),
+              detector_colors=('red','blue'), ewald_steps=100)
 
 view = View()
 